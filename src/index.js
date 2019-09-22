ccb9576367e1efb104e9aced3dbcd1c8401b17c4
chingsley <eneja.kc@gmail.com>
2019-09-22 13:27:48 +0100

    implement an hoc component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,56 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const HelloWorld = () => <h1>Higher Order Components</h1>;
-const Button = () => <button className="btn btn-primary">Press button</button>;
+const HelloWorld = props => 
+    <h1 count={props.count} onClick={props.incrementCounter}>
+      {props.hello} {props.count}
+    </h1>;
+
+const Button = props => 
+    <button 
+      className="btn btn-primary"
+      onClick={props.incrementCounter}
+    >
+      {props.buttonText} {props.count}
+    </button>;
+
+const higherOrderComponent = WrappedComponent =>
+  class extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        hello: 'Higher order Component',
+        buttonText: 'My Button',
+        counter: 0
+      }
+    }
+
+    incrementCounter = () => {
+      console.log('called!')
+      this.setState((prevState, props) => {
+        console.log(prevState.counter)
+        return {
+          counter: prevState.counter + 1
+        }
+      })
+    }
+
+    render() {
+      return <WrappedComponent 
+                hello={this.state.hello}
+                buttonText={this.state.buttonText}
+                count={this.state.counter}
+                incrementCounter={this.incrementCounter}
+              />
+    }
+  }
+
+
+const HOCHelloWorld = higherOrderComponent(HelloWorld);
+const HOCButton = higherOrderComponent(Button);
 
 const App = () => {
-  return [<HelloWorld key="1" />, <Button key="2" />];
+  return [<HOCHelloWorld key="1" />, <HOCButton key="2" />];
 };
 
 const rootElement = document.getElementById("root");
